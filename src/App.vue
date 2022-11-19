269dba4dcf26ebb70646b3586b0efdf545286b32
Aryaman Mishra <aryaman.mishra73@gmail.com>
2022-11-19 15:02:30 +0530

    added a navigator module

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <PlannerItem/>
+  </div>
+</template>
+
+<script>
+import PlannerItem from './views/PlannerItem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PlannerItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+  body {
+    margin: 0;
+  }
+</style>
